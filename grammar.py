2758d19592abe241fa76e949cbc64a671f2477e2
Paul Marande <paul.marande@efrei.net>
2022-04-23 18:15:28 +0200

    [GLOBAL] - Modification of grammar

--- grammar.py
+++ grammar.py
@@ -1,30 +1,49 @@
+
 class Grammar:
     def __init__(self, path):
         with open(path, 'r') as file:
             self.regle = {}
+            self.terminaux = []
             lines=file.read().splitlines()
             for line in lines:
                 line = line.split("->")
                 line[0] = line[0].strip()
                 line[1] = line[1].strip()
-                line[1] = line[1].split("|")
+                line[1] = list(set(line[1].split("|")))
                 for index, item in enumerate(line[1]):
                     line[1][index] = item.strip()
+                    if(line[1][index] != 'eps'):
+                        line[1][index] = list(line[1][index])
+                        for index2, _ in enumerate(line[1][index]):
+                            line[1][index][index2] = list(line[1][index][index2])
+                            if(line[1][index][index2] not in self.terminaux):
+                                self.terminaux.append(line[1][index][index2])
+                    else:
+                        line[1][index] = [['eps']]
+                        if(['esp'] not in self.terminaux):
+                            self.terminaux.append(['eps'])
                 try :
-                    self.regle[line[0]] += list(set(line[1]))
+                    self.regle[line[0]] += line[1]
                 except KeyError:
-                    self.regle[line[0]] = list(set(line[1]))
+                    self.regle[line[0]] = line[1]
+            self.non_terminaux = [[key] for key, _ in self.regle.items()]
+            for item in self.non_terminaux:
+                if item in self.terminaux:
+                    self.terminaux.remove(item)
 
 
     def __str__(self):
         result = ""
         for key, values in self.regle.items():
-            result += f"{key} ->"
+            result += f"{key} -> "
             for index, item in enumerate(values):
+                for index2, _ in enumerate(item):
+                    result += f"{item[index2][0]}"
                 if (index == len(values)-1):
-                    result += f" {item}\n"
+                    result += f"\n"
                 else:
-                    result += f" {item} |"
+                    result += f" | "
+        print(self.regle)
         return result
 
 
@@ -34,22 +53,23 @@ class Grammar:
         for key, values in self.regle.items():
             sup=[]
             for item in values:
-                if item.startswith(key) : 
+                if item[0] == [key]: 
                     recursive = True 
                     sup.append(item)
+                    new_item = item[1:]
+                    new_item.append([f'{key}\''])
                     try:
-                        temp[f'{key}\''].append(f'{item[len(key):]}{key}\'')
+                        temp[f'{key}\''].append(new_item)
                     except KeyError:
-                        temp[f'{key}\''] = ([f'{item[len(key):]}{key}\''])
+                        temp[f'{key}\''] = ([new_item])
             for index, item in enumerate(values): 
-                if recursive==True and not item.startswith(key):
-                    values[index]+=f'{key}\''
+                if recursive==True and not item[0] == [key] and not item[0] == ['eps']: # A VERFIER
+                    values[index].append([f'{key}\''])
             recursive=False
             for item in sup:
                 values.remove(item)
                 if len(self.regle[key]) == 0:
-                    values.append(f"{key}\'") 
+                    values.append([[f"{key}\'"]]) 
         for _, values in temp.items():
-            values.append("eps")
-            values=list(set(values))
+            values.append([["eps"]])
         self.regle = dict(self.regle, **temp)
