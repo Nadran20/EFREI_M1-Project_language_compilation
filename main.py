bd2103c6b3c574aa91cb14570914953f2f61a80d
Nadran20 <71322381+Nadran20@users.noreply.github.com>
2022-04-26 11:01:25 +0200

    Add first with 1 level of recursive

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from grammar import Grammar
 
 if __name__ == '__main__':
-    test1 = Grammar('src/test.txt')
+    test1 = Grammar('src/test2')
     print('Lecture du langage :')
     print(test1)
     test1.remove_left_recursive()
     print('Suppression de la récursivité gauche :')
     print(test1)
-    test1.get_first()
+
+
+    print('Premier du langage attendu :')
+    print("{'X': ['a'], ['eps'], ['b'], ['u'], 'Y' : ['a'], ['u'], ['eps'], 'Z': ['a'], ['b'], ['eps'], 'A': [['a'], ['eps']], 'B': [['u']]}" )
+
+    print('Premier du langage obtenu :')
+    print(test1.get_first())
+
